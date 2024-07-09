a1eb50d6d6e6e6431dbd0556398afc3c8bac3cb9
TobiLean <Tobiloba.tsl@gmail.com>
2024-07-09 19:11:23 +0100

    change route handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/cart",
-    element: <Cart />
-  },
-  {
-    path: "/checkout",
-    element: <Checkout />
-  },
-])
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from 'react-router-dom';
+
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />
+//   },
+//   {
+//     path: "/cart",
+//     element: <Cart />
+//   },
+//   {
+//     path: "/checkout",
+//     element: <Checkout />
+//   },
+// ])
 
 import './App.css';
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="checkout" element={<Checkout />} />
+    </Routes>
+  </Router>
+  )
 }
 
 export default App
